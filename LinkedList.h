1fe56821f49b3265a9b4ddf3909c1a07576a0925
Jacob Mccorkle <gjv4hf@ludwig.mcs.uvawise.edu>
2026-09-17 20:18:10 -0400

    Updated errors in LinkedList file

--- LinkedList.h
+++ LinkedList.h
@@ -1,230 +1,253 @@
 #ifndef LINKEDLIST_H
 #define LINKEDLIST_H
 
+#include <iostream>
+using namespace std;
+
 template <typename T>
-class LinkedList {
- private:
-  struct Node {
-    T value;
-    Node* next;
-
-    Node(const T& v, Node* n = nullptr)
-    {
-
-      this->value = value;
-      this->next = next
-
-	}
-
-  };
-
-  Node* head;
-  int size;
-
-public:
-  LinkedList()
-  {
-    head = nullptr;
-    count = 0;
-  }
-
-  LinkedList(const LinkedList& other)
-  {
-    head = nullptr;
-    count = 0
-
-      Node* current = other.head;
-
-    while (current != nullptr)
-      {
-	pushBack(current->value);
-	current = current->next;
-      }
-  };
-  
-
-  LinkedList& operator=(const LinkedList& other)
-  {
-    if (this != &other)
-      {
-
-	while (head != nullptr)
-	  {
-	    popFront();
-	  }
-
-	Node* current = other.head;
-
-	while (current != nullptr)
-	  {
-	    pushBack(current->value);
-	    current = current;
-	      }
-      }
-    return *this;
-  }
-
-  ~LinkedList()
-  {
-    while (head != nullptr)
-      {
-	popFront();
-      }
-  }
-
-
-  void pushFront(const T& value)
-  {
-    Node* newNode = new Node(value);
-
-    if (head == nullptr)
-      {
-	head = newNode;
-      }
-    else
-      {
-	Node* current = head;
-
-	while (current->next != nullptr)
-	  {
-	    current = current->next;
-	  }
-
-	current->next = newNode;
-      }
-
-    count++;
-  }
-
-  void popFront()
-  {
-    if (head == nullptr)
-      {
-	return;
-      }
-
-    Node* temp = head;
-    head = head->next;
-
-    delete temp;
-    count--;
-  }
-
-  bool remove(const T& value)
-  {
-    if (head == nullptr)
-      {
-	return false;
-      }
-
-    if (head->value == value)
-      {
-	popFront();
-	return true;
-      }
-
-    Node* current = head;
-
-    while (current->next != nullptr)
-      {
-	if (current->next->value == value)
-	  {
-	    Node* temp = current->next;
-	    current->next = temp->next;
-
-	    delete temp;
-	    count--;
-
-	    return true;
-	  }
-
-	current = current->next;
-
-      }
-
-    return false;
-  }
-
-
-  bool contains(const T& value) const
-  {
-    Node* current = head;
-
-    while (current != nullptr)
-      {
-	if (current->value == value)
-	  {
-	    return true;
-	  }
-
-	current = current->next;
-      }
-
-	return false;
-  }
-
-  int size() const
-  {
-    return count;
-  }
-
-  bool isEmpty() const
-  {
-    return head == nullptr;
-  }
-
-  void print() const
-  {
-
-    Node* current = head;
-    
-    while (head != nullptr)
-      {
-	cout << current->value << " ";
-	current = current->value;
-      }
-
-    count << endl;
-  }
-
-class Iterator
+class LinkedList
 {
 private:
-  Node* current;
+
+    struct Node
+    {
+        T value;
+        Node* next;
+
+        Node(const T& value, Node* next = nullptr)
+        {
+            this->value = value;
+            this->next = next;
+        }
+    };
+
+    Node* head;
+    int count;
 
 public:
 
-  Iterator(Node* node)
-  {
-    current = node;
-  }
+    // Constructor
+    LinkedList()
+    {
+        head = nullptr;
+        count = 0;
+    }
 
-  Iterator& operator++()
-  {
-    current = current->;
-    return *this;
-  }
+    // Push to front
+    void pushFront(const T& value)
+    {
+        head = new Node(value, head);
+        count++;
+    }
 
-  T& operator*()
-  {
-    return current->value;
-  }
+    // Push to back
+    void pushBack(const T& value)
+    {
+        Node* newNode = new Node(value);
 
-  bool operator!=(const Iterator& other) const
-  {
-    return current != other.current;
-  }
+        if (head == nullptr)
+        {
+            head = newNode;
+        }
+        else
+        {
+            Node* current = head;
+
+            while (current->next != nullptr)
+            {
+                current = current->next;
+            }
+
+            current->next = newNode;
+        }
+
+        count++;
+    }
+
+    // Pop from front
+    void popFront()
+    {
+        if (head == nullptr)
+        {
+            return;
+        }
+
+        Node* temp = head;
+        head = head->next;
+
+        delete temp;
+        count--;
+    }
+
+    // Remove a value
+    bool remove(const T& value)
+    {
+        if (head == nullptr)
+        {
+            return false;
+        }
+
+        if (head->value == value)
+        {
+            popFront();
+            return true;
+        }
+
+        Node* current = head;
+
+        while (current->next != nullptr)
+        {
+            if (current->next->value == value)
+            {
+                Node* temp = current->next;
+                current->next = temp->next;
+
+                delete temp;
+                count--;
+
+                return true;
+            }
+
+            current = current->next;
+        }
+
+        return false;
+    }
+
+    // Check if value exists
+    bool contains(const T& value) const
+    {
+        Node* current = head;
+
+        while (current != nullptr)
+        {
+            if (current->value == value)
+            {
+                return true;
+            }
+
+            current = current->next;
+        }
+
+        return false;
+    }
+
+    // Return size
+    int size() const
+    {
+        return count;
+    }
+
+    // Check if empty
+    bool isEmpty() const
+    {
+        return head == nullptr;
+    }
+
+    // Print list
+    void print() const
+    {
+        Node* current = head;
+
+        while (current != nullptr)
+        {
+            cout << current->value << " ";
+            current = current->next;
+        }
+
+        cout << endl;
+    }
+
+    // Destructor
+    ~LinkedList()
+    {
+        while (head != nullptr)
+        {
+            popFront();
+        }
+    }
+
+    // Copy Constructor
+    LinkedList(const LinkedList& other)
+    {
+        head = nullptr;
+        count = 0;
+
+        Node* current = other.head;
+
+        while (current != nullptr)
+        {
+            pushBack(current->value);
+            current = current->next;
+        }
+    }
+
+    // Assignment Operator
+    LinkedList& operator=(const LinkedList& other)
+    {
+        if (this != &other)
+        {
+            while (head != nullptr)
+            {
+                popFront();
+            }
+
+            Node* current = other.head;
+
+            while (current != nullptr)
+            {
+                pushBack(current->value);
+                current = current->next;
+            }
+        }
+
+        return *this;
+    }
+
+    // Iterator
+    class Iterator
+    {
+    private:
+        Node* current;
+
+    public:
+
+        Iterator(Node* node)
+        {
+            current = node;
+        }
+
+        // Prefix ++
+        Iterator& operator++()
+        {
+            current = current->next;
+            return *this;
+        }
+
+        // Dereference *
+        T& operator*()
+        {
+            return current->value;
+        }
+
+        // Not equal !=
+        bool operator!=(const Iterator& other) const
+        {
+            return current != other.current;
+        }
+    };
+
+    // Begin iterator
+    Iterator begin()
+    {
+        return Iterator(head);
+    }
+
+    // End iterator
+    Iterator end()
+    {
+        return Iterator(nullptr);
+    }
 };
 
-  Iterator begin()
-  {
-    return Iterator(head);
-  }
-
-  Iterator end()
-  {
-    return Iterator(nullptr);
-  }
-
-};
 #endif
